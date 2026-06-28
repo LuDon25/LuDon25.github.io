f8fa94242e831e06ee891b1f9fed85fd9e8e0c6a
LuDon25 <beulah.ludon1@gmail.com>
2026-06-28 21:53:51 +0300

    Fix the contact form

--- main.js
+++ main.js
@@ -22,50 +22,7 @@ function prevSlide() {
 // Auto slide every 5 seconds
 setInterval(nextSlide, 5000);
 
-//About me content toggle
-var tablinks = document.getElementsByClassName("tab-links");
-var tabcontents = document.getElementsByClassName("tab-contents");
-
-function opentab(tabname) {
-    for (tablink of tablinks) {
-        tablink.classList.remove("active-link");
-    }
-
-    for (tabcontent of tabcontents) {
-        tabcontent.classList.remove("active-tab");
-    }
-    event.currentTarget.classList.add("active-link");
-    document.getElementById(tabname).classList.add("active-tab");
-}
-
-//for google sheets
-const scriptURL = 'https://script.google.com/macros/s/AKfycbxD3gUubnLHc7EGVQSd47D7BhFlw0V9Tm0gboo_ONuasvRk0ez_9v-EUdHhaPZr1CcV/exec'
-const form = document.forms['submit-to-google-sheet']
-const msg = document.getElementById("msg");
-
-form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-        .then(response => {
-            msg.innerHTML = "Sent Successfully!";
-            setTimeout(function () {
-                msg.innerHTML = "";
-            }, 3000);
-            form.reset();
-        })
-        .catch(error => console.error('Error!', error.message))
-});
-
-//submit button animation
-const submitBtn = document.getElementById("submitbtn");
-
-submitBtn.addEventListener("click", function () {
-    submitBtn.classList.add("clicked");
-    setTimeout(function () {
-        submitBtn.classList.remove("clicked");
-    }, 300);
-});
-
+// About-Me section functionality
 const aboutmeTabs = document.querySelectorAll('.aboutme-tab');
 const aboutmeContents = document.querySelectorAll('.aboutme-tab-content');
 
@@ -142,3 +99,5 @@ document.addEventListener('keydown', e => {
     }
 
 });
+
+
